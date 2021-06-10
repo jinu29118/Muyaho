23d9f457cf08a3cd2d53c38426bceb2d577a9b8e
강현수 <2468abcdee@naver.com>
2021-06-11 02:10:10 +0900

    사용할 변수 추가. Maps는 사용안할것.

--- TheWisdomofOurs/app/src/main/java/com/example/thewisdomofours/AddClassActivity.kt
+++ TheWisdomofOurs/app/src/main/java/com/example/thewisdomofours/AddClassActivity.kt
@@ -12,10 +12,8 @@ class AddClassActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_class)
         init()
-        //fasdf
-        //123123
-    }
 
+    }
     private fun init(){
         val cancelBtn = findViewById<Button>(R.id.cancelBtnMake)
         val createBtn = findViewById<Button>(R.id.createBtnMake)
