a6142fe72a4f4cb14c1722e521e3b7ca628e7abd
menbung <zim2535@naver.com>
2021-06-16 02:37:43 +0900

    Add RecommandActivity

--- TheWisdomofOurs/app/src/main/java/com/example/thewisdomofours/RecommandActivity.kt
+++ TheWisdomofOurs/app/src/main/java/com/example/thewisdomofours/RecommandActivity.kt
@@ -2,19 +2,56 @@ package com.example.thewisdomofours
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.thewisdomofours.databinding.ActivityRecommandBinding
 
 class RecommandActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRecommandBinding
+    lateinit var adapter: MyRecyclerAdapter
+
+    var recommandArr = arrayListOf<MySubLectureData>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding=ActivityRecommandBinding.inflate(layoutInflater)
+        binding=ActivityRecommandBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initData()
+        init()
+    }
 
-        val intent=Intent(this,MainActivity::class.java)
-
+    private fun init(){
+        binding!!.recommandRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = MyRecyclerAdapter(recommandArr)
+        binding!!.recommandRecycler.addItemDecoration(DividerItemDecoration(this, 1))
+        adapter.itemClickListener = object :MyRecyclerAdapter.OnItemClickListener{
+            override fun OnItemClick(
+                    holder: MyRecyclerAdapter.ViewHolder,
+                    view: View,
+                    data: MySubLectureData,
+                    position: Int
+            ) {
+                val intent = Intent(this@RecommandActivity, DetailedLectureActivity::class.java)
+                intent.putExtra("id", data.class_id)
+                startActivity(intent)
+            }
+        }
+        binding!!.recommandRecycler.adapter = adapter
         binding.next.setOnClickListener{
+            val intent=Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun initData() {
+        recommandArr.add(MySubLectureData(-1, "Make Kimchi",
+                "Let's learn how to make korea traditional food", "Food"))
+        recommandArr.add(MySubLectureData(2, "Vocal Lesson",
+                "Learn the basic of vocalization", "Music"))
+        recommandArr.add(MySubLectureData(3, "Painting Class",
+                "Learn portrait painting", "Drawing"))
+        recommandArr.add(MySubLectureData(4, "A small tip in gardening",
+                "Why you need a pond", "Gardening"))
     }
 }
